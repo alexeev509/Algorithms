040bbe83c61ecd0dcf2b73725f92dfc5939e6f9b
Alekseev Yaroslav <alekseevy509@gmail.com>
2020-06-30 21:09:34 +0300

    First variant of decision

--- src/main/java/codeforces/Task_13C.java
+++ src/main/java/codeforces/Task_13C.java
@@ -22,7 +22,39 @@ public class Task_13C {
     }
 
     public static int findAnswer() {
-        //NOP
-        return -1;
+        int[] first_path = new int[size];
+        int[] second_path = new int[size];
+
+        if (size > 1) {
+            first_path[0] = 0;
+            second_path[0] = (sequence[1] - sequence[0]);
+
+            first_path[1] = (sequence[0] - sequence[1]);
+            second_path[1] = 0;
+        }
+
+        for (int i = 2; i < sequence.length; i++) {
+            if (sequence[i] < sequence[i - 1] + first_path[i - 1]) {
+                first_path[i] = sequence[i - 1] + first_path[i - 1] - sequence[i];
+            }
+            if (sequence[i] < sequence[i - 1] + second_path[i - 1]) {
+                second_path[i] = sequence[i - 1] + second_path[i - 1] - sequence[i];
+            }
+        }
+
+        int answer = getMaxSumm(first_path, second_path);
+        System.out.println(answer);
+        return answer;
+    }
+
+    public static int getMaxSumm(int[] first_path, int[] second_path) {
+        int summ1 = 0;
+        int summ2 = 0;
+
+        for (int i = 0; i < size; i++) {
+            summ1 += Math.abs(first_path[i]);
+            summ2 += Math.abs(second_path[i]);
+        }
+        return Math.min(summ1, summ2);
     }
 }
